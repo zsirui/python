a9789d7ace8c7aaa1e9b25f19c3b6539e9a8862f
zsirui <zsirui@126.com>
2016-04-11 13:27:44 +0800

    a lib include md5 and SHA algorithms

---
+++ hash.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+import hashlib, sys
+from os.path import isfile
+from zlib import crc32
+
+class HASH():
+	"""docstring for hash"""
+	def __init__(self, src):
+		self.src = src
+
+	def Compare(self, src, target):
+		if src == target:
+			return True
+		else:
+			return False
+		
+
+class MD5(HASH):
+	def __init__(self, src):
+		HASH.__init__(self, src)
+		self.value = self.md5Value(self.src)
+
+	def md5Value(self, src):
+		m = hashlib.md5()
+		if isfile(src):
+			with open(src, 'rb') as fh:
+				while True:
+					data = fh.read(8192)
+					if not data:
+						break
+					m.update(data)
+		else:
+			m.update(src)
+		return m.hexdigest()
+		
+class CRC32(HASH):
+	"""docstring for CRC32"""
+	def __init__(self, src):
+		HASH.__init__(self, src)
+		self.value = self.crcValue(self.src)
+		
+	def crcValue(self, src):
+		crc = 0
+		if isfile(src):
+			with open(src, 'rb') as fh:
+				while True:
+					data = fh.read(8192)
+					if not data:
+						break
+					crc = crc32(data, crc)
+		else:
+			crc = crc32(src, crc)
+		return str(hex(crc & 0xffffffff))[2:]
+
+class SHA(HASH):
+	"""docstring for SHA"""
+	def __init__(self, src, Type):
+		HASH.__init__(self, src)
+		self.type = self.setType(Type)
+		self.value = self.shaValue(src, self.type)
+
+	def setType(self, Type):
+		typelist = [['SHA1', hashlib.sha1()], ['SHA224', hashlib.sha224()], ['SHA256', hashlib.sha256()], ['SHA384', hashlib.sha384()], ['SHA512', hashlib.sha512()]]
+		for t, m in typelist:
+			if Type.upper() == t:
+				return m
+		return typelist[0][1]
+
+	def shaValue(self, src, Type):
+		s = Type
+		if isfile(src):
+			with open(src, 'rb') as fh:
+				while True:
+					data = fh.read(8192)
+					if not data:
+						break
+					s.update(data)
+		else:
+			s.update(src)
+		return s.hexdigest()
